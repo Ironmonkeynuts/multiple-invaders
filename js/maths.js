2916fdb703335670ec8a6fcd5c4dbefa164cebe0
David Doyle-Owen <thinkimagine@outlook.com>
2026-03-12 16:28:45 +0000

    Update maths.js for clearer code

--- js/maths.js
+++ js/maths.js
@@ -1,17 +1,18 @@
-function isFactor(bomb, target) {
-    if (bomb === 0) {
+function isFactor(bombValue, targetNumber) {
+    if (bombValue === 0) {
         return false;
     }
-    return target % bomb === 0;
+
+    return targetNumber % bombValue === 0;
 }
 
-function isPrime(num) {
-    if (num < 2) {
+function isPrime(number) {
+    if (number < 2) {
         return false;
     }
 
-    for (let i = 2; i <= Math.sqrt(num); i += 1) {
-        if (num % i === 0) {
+    for (let i = 2; i <= Math.sqrt(number); i += 1) {
+        if (number % i === 0) {
             return false;
         }
     }
@@ -19,16 +20,16 @@ function isPrime(num) {
     return true;
 }
 
-function isPrimeFactor(bomb, target) {
-    return isPrime(bomb) && isFactor(bomb, target);
+function isPrimeFactor(bombValue, targetNumber) {
+    return isPrime(bombValue) && isFactor(bombValue, targetNumber);
 }
 
-function calculateDamage(bomb, target) {
-    if (!isFactor(bomb, target)) {
+function calculateDamage(bombValue, targetNumber) {
+    if (!isFactor(bombValue, targetNumber)) {
         return 0;
     }
 
-    if (isPrimeFactor(bomb, target)) {
+    if (isPrimeFactor(bombValue, targetNumber)) {
         return 2;
     }
 
